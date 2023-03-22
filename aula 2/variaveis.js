c4b7c967a5b5c297c816c4c3e095260051d99dfc
MuriloBC2 <murilobarrosdocarmo@gmail.com>
2023-03-22 08:29:38 -0300

    aula 2, variaveis

---
+++ aula 2/variaveis.js
@@ -0,0 +1,28 @@
+//  let num1 = 5
+// const num2= 3
+// var num3 = 7
+// var msg = "Ola amigos ! Esse é o número: "
+
+// console.log(msg + num2)
+// console.log(num1+num2)
+
+// console.log(typeof num1)
+// console.log(typeof msg)
+
+// JSON- JavaScript Object Notion
+
+// let usuario = {
+//     name: "Murilo",
+//     idade: 17,
+//     calvo:  false, 
+
+// }
+
+// console.log(usuario)
+
+const nomes = ["Joao" , "paulo" , "cleber"]
+
+console.log (nomes)
+console.log (typeof nomes)
+console.log (nomes[0])
+
